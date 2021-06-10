36e14a00db80c3426addd50772152d3ed92eccdd
Hemanth <jampala.hemanth26@gmail.com>
2021-06-10 17:08:16 +0530

    basic project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello hr-cyberMall</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
